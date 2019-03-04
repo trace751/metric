a89e5fb6acb88197c88ccd1e4dd456d8033967d3
Ram Dettmer <ramdettmer751@gmail.com>
2019-03-03 22:46:29 -0800

    Add files via upload

---
+++ page-home.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div id="content" class="site-content">
+	<main class="hero-title">
+		<h1>My Title</h1>
+		<p>My subtitle</p>
+	</main>
+</div><!-- #page -->
+<div class="home-wrap">
+	<h1>IPSUM FEUGIAT CONSEQUAT</h1>
+	<p>Tempus adipiscing commodo ut aliquam blandit</p>
+</div>
+
+
+
+<?php get_footer(); ?>
